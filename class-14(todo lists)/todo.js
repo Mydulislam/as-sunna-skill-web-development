31cff95b027afc2bb69293555a75d36b75d7f11b
Mydulislam <mydulislam693852@gmail.com>
2023-09-05 12:13:34 +0600

    class-14 inserted

---
+++ class-14(todo lists)/todo.js
@@ -0,0 +1,7 @@
+/* 
+    Selections part start
+*/
+let inputField = document.getElementById('inputField')
+let todoForm = document.querySelector('form');
+let uncompleteTodoUl = document.querySelector('.uncompleteTodo ul')
+let completeUl = document.querySelector('.completedTodo ul')
